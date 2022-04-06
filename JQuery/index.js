a66b24d17c03c6f415af93de28ac5043f4aa2b6b
Patrick-FRE <patrick.lin@antra.com>
2022-04-06 15:47:20 -0400

    Update Jquery

---
+++ JQuery/index.js
@@ -0,0 +1,64 @@
+
+class PQuery {
+    constructor(selector) {
+        this.elements = document.querySelectorAll(selector);
+    }
+    html(innerhtml) {
+        this.elements.forEach(element => {
+            element.innerHTML = innerhtml;
+        })
+    }
+    hide() {
+        this.elements.forEach(element => {
+            element.preDisplay = element.style.display;
+            element.style.setProperty('display', 'none');
+        })
+    }
+    on(eventType, cb) {
+        this.elements.forEach(element => {
+            element.addEventListener(eventType, cb)
+
+        })
+    }
+    show() {
+        this.elements.forEach(element => {
+            element.style.setProperty('display', element.preDisplay);
+        })
+    }
+}
+
+
+const $$ = (selector) => {
+    return new PQuery(selector)
+}
+
+$$.ajax = () => {
+    console.log('ajax')
+}
+
+// const test = $$('button.continue')
+
+// test.html('Next Step...');
+const btn = $$('button.continue')
+
+btn.html('Next Step...!!!');
+
+
+var hiddenBox = $$("#banner-message");
+hiddenBox.hide();
+
+console.log(btn.html == hiddenBox.html)
+
+$$("#button-container button").on("click", function (event) {
+    hiddenBox.show();
+});
+
+
+
+$$.ajax({
+    url: "https://jsonplaceholder.typicode.com/todos/1",
+    success: function (result) {
+        console.log(result)
+    }
+});
+
